566b857ae052c6da7ca1fcf0c4e1d83499961ce6
rsego-hub <rsego@purdue.edu>
2023-01-26 19:57:49 -0500

    file creation and skeleton code for metric

--- metric.ts
+++ metric.ts
@@ -1,10 +1,16 @@
 let message: string = 'Hello, World!';
 console.log(message);
 
-class Metric {
-    
+// Metric class itself should never be created, only subclasses which have 
+// specific metric calculations
+abstract class Metric {
+    get_metric(repo: Repository) {
+    }
 }
 
+
 class LicenseMetric extends Metric {
-    
+    get_metric(repo: Repository) {
+
+    }
 }
