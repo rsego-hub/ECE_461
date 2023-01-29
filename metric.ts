9da1b1875d674207aef0e1c32a9bc7754faf3fe2
rsego-hub <rsego@purdue.edu>
2023-01-29 10:47:49 -0500

    licensemetric explicit typing and metric class comments

--- metric.ts
+++ metric.ts
@@ -1,17 +1,23 @@
 import { Repository } from "./repository.js"
 
 /* Metric Class
- *
+ * Abstract class to be extendedd for all current and future metric subclass
+ * 
+ * Subclasses:
+ * License, BusFactor, RampUp, ResponsiveMaintainer, Correctness
 */
 abstract class Metric {
     get_metric(repo: Repository) {
     }
 }
 
-
+/* License Class
+ * Finds compatibility of repository license with GNU Lesser General Public License v2.1
+ *
+*/
 class LicenseMetric extends Metric {
     get_metric(repo: Repository) {
-        var license = repo.get_license(); // @type {string}
+        var license: string = repo.get_license();
 
         let regex = new RegExp("LGPL v2.1") // Very basic regex, can be expanded on in future
         if(regex.test(license)) {
