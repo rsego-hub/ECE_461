0e4aae3ededa2e39b226fdec9fec4e0e05031023
rsego-hub <rsego@purdue.edu>
2023-01-28 23:00:02 -0500

    removed old files and added basic regex to license metric

--- metric.ts
+++ metric.ts
@@ -1,8 +1,9 @@
-let message: string = 'Hello, World!';
-console.log(message);
-
-// Metric class itself should never be created, only subclasses which have 
-// specific metric calculations
+/* Metric Class
+ *
+ * 
+ *
+ *
+*/
 abstract class Metric {
     get_metric(repo: Repository) {
     }
@@ -11,6 +12,12 @@ abstract class Metric {
 
 class LicenseMetric extends Metric {
     get_metric(repo: Repository) {
+        var license = repo.get_license(); // @type {string}
 
+        let regex = new RegExp("LGPL v2.1") // Very basic regex, can be expanded on in future
+        if(regex.test(license)) {
+            return true
+        }
+        return false
     }
 }
