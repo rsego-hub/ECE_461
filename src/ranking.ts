83ab53b505dc1bbd1ce09055bf414d7fb996a9dd
rsego-hub <rsego@purdue.edu>
2023-02-07 00:54:19 -0500

    added preliminary ranking display

--- src/ranking.ts
+++ src/ranking.ts
@@ -1,3 +1,39 @@
+import { Repository } from "./repository"
+import { Metric } from "./metric"
+
 class Ranking {
-    
+    public display_ranking(repolist: RepositoryList) {
+        
+        var repo_score_map = new Map() // Initialize map
+        for (var repo in repolist) { // fill table with weighted scores
+            repo_score_map.set(repo, Metric.get_weighted_sum(repo)); 
+        }
+
+        // do once for every repo in list
+        for (var i = 0; i < repolist.length; i++) {
+            var max_score = -1; // holders for max vals
+            var max_repo = null;
+            
+            // go through every key in map repo -> score
+            for(var key in repo_score_map.keys()) {
+                var new_val = repo_score_map.get(key);
+                if(new_val > max_score) { // get max score and associated repo
+                    max_score = new_val;
+                    max_repo = key;
+                }
+            
+            // fail if max_repo is null
+            if(max_repo == null) {
+                console.log('debug', "Null repo found");
+                return
+            }
+
+            // print to console
+            console.log('info', "Issue #%d: %s", max_repo.title,);
+            repo_score_map.delete(max_repo)
+            }
+        }
+        
+    } 
+
 }
