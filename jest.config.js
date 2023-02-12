8b961b054ca85882f8804aca6e008af02fd8d9d9
Priyanka Pereira <papereir@purdue.edu>
2023-02-12 12:26:29 -0500

    Node to version 18.14.0, Jest reporter changed to v8

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  "collectCoverageFrom": ["./src/**"],
+  "collectCoverageFrom": ["./src/**/*.ts"],
   "collectCoverage": true,
+  "coverageProvider": 'v8',
   roots: ['./tests', './src'],
-  coverageReporters: ['json-summary', 'text', 'html-spa'],
+  coverageReporters: ['json-summary', 'text', 'html-spa', 'lcov'],
   reporters: ['./build/tests/jest_reporter.js'],
 };
 // 'text', 'json', 'text-summary', 
