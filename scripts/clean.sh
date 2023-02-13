b4b6c849dc444e6c51e33257220f0984bd3aea51
Priyanka Pereira <papereir@purdue.edu>
2023-02-12 19:35:40 -0500

    Updated clean.sh for full cleanup

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,7 +4,7 @@ if [[ "$1" == "all" ]]; then
 	rm -rf ~/.npm
 	rm -rf ~/.nvm
 	rm -rf node_modules
-	rm -rf build coverage logs downloads	
+	rm -rf build coverage logs downloads local_clones
 else
-	rm -rf build coverage logs downloads	
+	rm -rf build coverage logs downloads local_clones
 fi
